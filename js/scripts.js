3e46d944af614f6555885b8c94badf0b61138843
David Cole <davidtcole@yahoo.com>
2020-08-22 12:12:47 -0700

    add a function to grab the user's numerical input and create an array of the appropriate length to later be processed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 //Business Logic
+function processInput(userInput) {
+  const numericalArray = [];
+
+  for(i = 0; i <= userInput; i++) {
+    numericalArray.push(i);
+  }
+  console.log(numericalArray);
+}
+
 
 //UI logic
 $(document).ready(function() {
   $("#input").submit(function(){
     event.preventDefault();
-    
+    $("#display-result").hide();
+
+    const userInput = $("input#user-input").val();
+
   });
 });
